2408dc89f230b120e9ce79dd7ee13b7246070ce8
kzherdinnn <herdinkz2003@gmail.com>
2026-02-06 14:42:48 +0700

    Add debug route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 // Route utama
 Route::get('/', function () {
@@ -25,3 +26,32 @@ Auth::routes();
 
 // Rute menuju home setelah login
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// DEBUG ROUTE - HAPUS NANTI
+Route::get('/debug-images', function() {
+    $results = [];
+
+    // 1. Cek Folder Public Storage Link
+    $publicStorage = public_path('storage');
+    $results['public_storage_path'] = $publicStorage;
+    $results['public_storage_exists'] = file_exists($publicStorage);
+    $results['public_storage_is_link'] = is_link($publicStorage);
+    
+    // 2. Cek Real Storage Path 
+    $storagePath = storage_path('app/public/posts');
+    $results['real_storage_path_posts'] = $storagePath;
+    $results['real_storage_exists'] = file_exists($storagePath);
+    
+    // 3. List File di Storage Posts
+    if (file_exists($storagePath)) {
+        $files = scandir($storagePath);
+        $results['files_in_posts'] = array_values(array_diff($files, ['.', '..']));
+    } else {
+        $results['files_in_posts'] = 'Folder not found';
+    }
+
+    // 4. Cek Permission
+    $results['storage_perms'] = substr(sprintf('%o', fileperms(storage_path('app/public'))), -4);
+
+    return response()->json($results);
+});
